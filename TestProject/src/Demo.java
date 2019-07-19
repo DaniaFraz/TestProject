f73a137ed09f9934c7b3b98a94f71e868a4e7ef6
OmarKAbdul <Omarkhan0727@gmail.com>
2019-07-19 01:26:39 -0400

    created method sayHello

--- TestProject/src/Demo.java
+++ TestProject/src/Demo.java
@@ -1,9 +1,12 @@
 
 public class Demo {
+	public static void sayHello() {
+		System.out.println("Hello");
+	};
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Demo.sayHello();
 	}
 
 }
