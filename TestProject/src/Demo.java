811fd3034541c602a6a14382a10e9f986499062c
OmarKAbdul <Omarkhan0727@gmail.com>
2019-07-19 01:32:12 -0400

    added welcome Method

--- TestProject/src/Demo.java
+++ TestProject/src/Demo.java
@@ -4,9 +4,14 @@ public class Demo {
 		System.out.println("Hello");
 	};
 
+	public static void sayWelcome() {
+		System.out.println("Welcome to Git");
+	};
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Demo.sayHello();
+		Demo.sayWelcome();
 	}
 
 }
